0dfd1c976b2114f61e423e262f18b924125d118a
jenessa johnson <johnson.jenessa@gmail.com>
2016-05-07 14:39:37 -0700

    deleted test

--- header.php
+++ header.php
@@ -17,7 +17,33 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
+
+<title>SCCA 2012</title>
+
+<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+
+<link rel="stylesheet" href="http://f.fontdeck.com/s/css/BYIdloiyr75z6gbZBPq6eYPYeJQ/www.2012.sccaportfolioshow.com/18201.css" type="text/css" />
+<link rel="stylesheet" href="http://f.fontdeck.com/s/css/BYIdloiyr75z6gbZBPq6eYPYeJQ/2012.sccaportfolioshow.com/18201.css" type="text/css" />
+<link href="css/reset.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/modernizr.js"></script>
 <?php wp_head(); ?>
 </head>
+
+
+<header class="header" id="absolute">
+<img id="rule" src="img/headerrule.png" width="890" height="50">
+<a id="flag"><img src="img/blackgif.gif" width="40" height="40"></a>
+<ul>
+<li><a href="/">Home</a></li>
+<li><a href="program.php">Program</a></li>
+<li id="spacer">&nbsp;</li>
+<li><a id="attend" href="students.php">Students</a></li>
+<li><a href="event.php">Attend</a></li>
+</ul>
+
+ </header>
 
 <body <?php body_class(); ?>>
